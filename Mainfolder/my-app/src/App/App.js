1736b9d5670341cde93decf62b22f8e479dfa793
uslasts <dragun-uslasts@ukr.net>
2019-10-08 17:26:44 +0300

    finished with bugs cause of slicker

--- Mainfolder/my-app/src/App/App.js
+++ Mainfolder/my-app/src/App/App.js
@@ -17,7 +17,12 @@ class App extends Component {
                 <Header></Header>
                 <Main></Main>
                 <Footer></Footer>
+                <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+                <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+                <script type="text/javascript" src="../slick/slick.min.js"></script>
+                <script type="" src="../index.js"></script>
             </div>
+
         )
     }
 }
